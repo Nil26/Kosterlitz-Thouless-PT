5048274efab15fd3515d693053b4e4ed409070d5
Shang Zhang <zhshang@umich.edu>
2016-11-29 13:59:55 -0500

    c++

---
+++ KT_PT/KT_PT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KT_PT
+//
+//  Created by Shang Zhang on 11/29/16.
+//  Copyright © 2016 Shang Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
